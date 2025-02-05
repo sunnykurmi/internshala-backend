c4b28d06cfa09a8cab537524c54269d7c13e9c07
sunnykurmi <sunnykurmiskks@gmail.com>
2025-02-05 15:50:18 +0530

    updated authentication

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,13 +1,42 @@
 const jwt = require("jsonwebtoken");
 const ErrorHandler = require("../utils/ErrorHandler");
 const { catchError } = require("../middlewares/catchError");
+const User = require("../models/studentmodel");
+const Employee = require("../models/employeModel");
 
 exports.isAuthenticated = catchError(async (req, res, next) => {
-  const { token } = req.cookies;
-  if (!token) {
-    return next(new ErrorHandler("please login to access the resource", 401));
-  }
-  const { id } = jwt.verify(token, process.env.JWT_SECRET);
-  req.id = id;
-  next()
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return next(
+            new ErrorHandler("Not Authorized. Login Again", 401)
+        );
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    if (!token) {
+        return next(
+            new ErrorHandler("Not Authorized. Login Again", 401)
+        );
+    }
+
+    try {
+        const { id } = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(id) || await Employee.findById(id);
+        
+        if (!user) {
+            return next(
+                new ErrorHandler("User not found", 404)
+            );
+        }
+
+        req.user = user;
+        req.id = id;
+        next();
+    } catch (error) {
+        return next(
+            new ErrorHandler(error.message, 401)
+        );
+    }
 });
--- utils/SendToken.js
+++ utils/SendToken.js
@@ -1,14 +1,19 @@
 exports.sendtoken = (student, statuscode, res) => {
-    const token = student.getjwttoken();
-    const cookieExpireDays = parseInt(process.env.COOKIE_EXPIRE);
-    if (isNaN(cookieExpireDays) || cookieExpireDays <= 0) {
-        return res.status(500).json({ message: 'Invalid COOKIE_EXPIRE configuration' });
-    }
-    const expirationDate = new Date();
-    expirationDate.setDate(expirationDate.getDate() + cookieExpireDays);
-    const options = {
-        expires: expirationDate,
-        httpOnly: true,
-    };
-    res.status(statuscode).cookie('token', token, options).json({ success: true, id: student._id, token });
+  const token = student.getjwttoken();
+  const cookieExpireDays = parseInt(process.env.COOKIE_EXPIRE);
+  if (isNaN(cookieExpireDays) || cookieExpireDays <= 0) {
+    return res
+      .status(500)
+      .json({ message: "Invalid COOKIE_EXPIRE configuration" });
+  }
+  const expirationDate = new Date();
+  expirationDate.setDate(expirationDate.getDate() + cookieExpireDays);
+  const options = {
+    expires: expirationDate,
+    httpOnly: true,
+  };
+  res
+    .status(statuscode)
+    .cookie("token", token, options)
+    .json({ success: true, id: student._id, token ,student });
 };
